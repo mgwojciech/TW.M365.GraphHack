3cf0fde3534d47e673d18b52c7d376f5e3a5b917
mgwojciechavenga <marcin.wojciechowski@avenga.com>
2023-03-11 13:32:38 +0100

    initial push

---
+++ TW.M365.GraphHack.Lib/Services/GraphPeopleService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TW.M365.GraphHack.Lib.Services
+{
+    public class GraphPeopleService : IPeopleService
+    {
+        protected HttpClient GraphClient { get; }
+        public GraphPeopleService(HttpClient graphClient)
+        {
+            GraphClient = graphClient;
+        }
+        public async Task<User> GetUser(string userId = null, bool loadPresence = false, bool loadPhoto = false)
+        {
+            string baseQuery = "https://graph.microsoft.com/v1.0/";
+            if (String.IsNullOrEmpty(userId))
+            {
+                baseQuery += "me";
+            }
+            else
+            {
+                baseQuery += userId;
+            }
+            Task<HttpResponseMessage> meResponse = GraphClient.GetAsync(baseQuery);
+            Task<HttpResponseMessage> myPhotoResponse = null;
+            Task<HttpResponseMessage> myPresenceResponse = null;
+            List<Task> tasks = new List<Task>()
+            {
+                meResponse
+            };
+            if (loadPhoto)
+            {
+                myPhotoResponse = GraphClient.GetAsync($"{baseQuery}/photo/$value");
+                tasks.Add(myPhotoResponse);
+            }
+            if (loadPresence)
+            {
+                myPresenceResponse = GraphClient.GetAsync($"{baseQuery}/presence");
+                tasks.Add(myPresenceResponse);
+            }
+            await Task.WhenAll(tasks.ToArray());
+            User result = meResponse.Result.Content.ReadFromJsonAsync<User>().Result;
+            if(loadPresence)
+            {
+                result.Presence = myPresenceResponse.Result.Content.ReadFromJsonAsync<Presence>().Result;
+            }
+            if (loadPhoto)
+            {
+                result.AdditionalData.Add("photoBytes", myPhotoResponse.Result.Content.ReadAsByteArrayAsync().Result);
+            }
+
+            return result;
+        }
+    }
+}
--- TW.M365.GraphHack/MauiProgram.cs
+++ TW.M365.GraphHack/MauiProgram.cs
@@ -1,22 +1,24 @@
 ﻿using Microsoft.Extensions.Logging;
 using TW.M365.GraphHack.Lib.Authentication;
 using TW.M365.GraphHack.Lib.HttpHandlers;
+using TW.M365.GraphHack.Lib.Services;
+using TW.M365.GraphHack.Views;
 
 namespace TW.M365.GraphHack;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-		builder.Services.AddSingleton<IAADClientAuthenticator>(services =>
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        builder.Services.AddSingleton<IAADClientAuthenticator>(services =>
         {
             ClientAppInfo clientAppInfo = new ClientAppInfo()
             {
@@ -30,17 +32,18 @@ public static class MauiProgram
 #endif
             return authService;
         });
-		builder.Services.AddSingleton<HttpClient>(services =>
-		{
+        builder.Services.AddSingleton<HttpClient>(services =>
+        {
             IAADClientAuthenticator aadClientAuthenticator = services.GetRequiredService<IAADClientAuthenticator>();
             return new HttpClient(new GraphHttpHandler(aadClientAuthenticator));
         });
-		builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<IPeopleService, GraphPeopleService>();
 
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
